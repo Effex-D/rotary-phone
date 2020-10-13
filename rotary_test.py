c23a0f39eac0f88f343817c40beb3ca2dc22e9e2
effex <effex.d@gmail.com>
2020-10-13 14:02:44 +0100

    Added funtionality for dialing 0 instead of 10, and to identify when switch is simply open (eg, dial is in process)

--- rotary_test.py
+++ rotary_test.py
@@ -9,7 +9,12 @@ def add_one(channel):
 
 def end_count(channel):
     global count
-    print("You dialed: {}".format(count))
+    if count == 0:
+        print("Switch opened (IE: You are dialing something)")
+    elif count == 10:
+        print("You dialed: 0")
+    else:
+        print("You dialed: {}".format(count))
     count = 0
 
 GPIO.setwarnings(False)
@@ -19,6 +24,6 @@ GPIO.setup(13,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.add_event_detect(15,GPIO.RISING,callback=add_one, bouncetime=75)
 GPIO.add_event_detect(13,GPIO.BOTH,callback=end_count, bouncetime=200)
 
-message = input("Press something to quit")
+message = input("Press something to quit\n")
 
 GPIO.cleanup()
